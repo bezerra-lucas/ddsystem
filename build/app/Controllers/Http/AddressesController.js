83a54570a101126414a8b87e3680866f50e64dcf
Lucas Bezerra <1lcs.bzrr@gmail.com>
2020-12-06 20:40:41 -0300

    -

--- build/app/Controllers/Http/AddressesController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AddressesController {
-}
-exports.default = AddressesController;
-//# sourceMappingURL=AddressesController.js.map
